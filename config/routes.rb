edeaae62219b35e763795665856c34c7d21d4179
Anna Bodnarchuk <vchytel319@gmail.com>
2019-01-07 10:45:41 +0200

    Add ability to like and unlike shout

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
      root 'dashboards#show'
   end
   root to: "homes#show"
-  resources :shouts, only: [:create, :show]
+  resources :shouts, only: [:create, :show] do
+    member do
+      post 'like', to: 'likes#create'
+      delete 'unlike', to: 'likes#destroy'
+    end
+  end
   resources :passwords, controller: "passwords", only: [:create, :new]
   resource :session, only: [:create]
 
