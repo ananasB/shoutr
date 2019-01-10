d4ca0b173cf009e5cf65de849cf6a57a4d60c619
Anna Bodnarchuk <vchytel319@gmail.com>
2019-01-10 16:13:57 +0200

    Add ability to follow other users
    Added follow_button helper
    Added following_relationship

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,10 @@ Rails.application.routes.draw do
   resource :session, only: [:create]
 
   resources :users, only: [:create, :show] do
+    member do
+      post 'follow', to: 'followed_users#create'
+      delete 'unfollow', to: 'followed_users#destroy'
+    end
     resource :password,
       controller: "clearance/passwords",
       only: [:create, :edit, :update]
